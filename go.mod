d1850fb5c2b7da99a69f06b9a22fb27d1bed4904
Gowtham Munukutla <gowtham.m81197@gmail.com>
2021-02-28 18:33:31 +0530

    migrations list added

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-pg/pg/v10 v10.7.7
 	github.com/iancoleman/strcase v0.1.3
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/olekukonko/tablewriter v0.0.5
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 )
--- pkg/list.go
+++ pkg/list.go
@@ -2,11 +2,13 @@ package pkg
 
 import (
 	"fmt"
+	"github.com/olekukonko/tablewriter"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-func ListMigrations() {
+func ListMigrations() error {
 	wd, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
@@ -17,11 +19,27 @@ func ListMigrations() {
 		fmt.Println(err)
 	}
 
+	var data [][]string
 	for _, f := range m {
 		fi, err := os.Stat(f)
 		if err != nil {
-			fmt.Println(err)
+			return err
 		}
-		fmt.Println(fi.ModTime())
+		if strings.Contains(f, "main.go") {
+			continue
+		}
+
+		data = append(data, []string{filepath.Base(f), fi.ModTime().Format("02-01-2006")})
 	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Migration", "Last Modified"})
+
+	for _, v := range data {
+		table.Append(v)
+	}
+
+	table.Render()
+
+	return nil
 }
