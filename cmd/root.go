47e62daf5ebabebb2e660f3ad90524b038613580
Gowtham Munukutla <gowtham.m81197@gmail.com>
2021-02-28 13:54:28 +0530

    add postgres store to keep track. WIP

--- cmd/root.go
+++ cmd/root.go
@@ -31,12 +31,7 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "go-migrate",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `An abstract migration framework for all types of migrations`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/fatih/color v1.7.0
+	github.com/go-pg/pg/v10 v10.7.7
 	github.com/iancoleman/strcase v0.1.3
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
