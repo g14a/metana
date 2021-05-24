7a7bc2bf89028bb3256815b57bc8f1a03a279370
Gowtham Munukutla <gowtham.m81197@gmail.com>
2021-05-24 11:32:29 +0530

    repair tests

--- pkg/core/migrate/updown_test.go
+++ pkg/core/migrate/updown_test.go
@@ -1,6 +1,7 @@
 package migrate
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 
@@ -9,18 +10,22 @@ import (
 
 func TestRun(t *testing.T) {
 	// Run full up
-	logs := Run("", "testdata", "../../..", 0, true)
+	logs, err := Run("", "testdata", "../../..", 0, true)
+	assert.NoError(t, err)
 	pkg.ExpectLines(t, logs, []string{"1621746399-InitSchema.go", "1621746406-AddIndexes.go", "1621746410-AddData.go"}...)
 
 	// Run full down
-	logs = Run("", "testdata", "../../..", int(time.Now().Unix()), false)
+	logs, err = Run("", "testdata", "../../..", int(time.Now().Unix()), false)
+	assert.NoError(t, err)
 	pkg.ExpectLines(t, logs, []string{"1621746410-AddData.go", "1621746406-AddIndexes.go", "1621746399-InitSchema.go"}...)
 
 	// Run up Until
-	logs = Run("AddIndexes", "testdata", "../../..", 0, true)
+	logs, err = Run("AddIndexes", "testdata", "../../..", 0, true)
+	assert.NoError(t, err)
 	pkg.ExpectLines(t, logs, []string{"1621746399-InitSchema.go", "1621746406-AddIndexes.go"}...)
 
 	// Run Down Until
-	logs = Run("AddIndexes", "testdata", "../../..", int(time.Now().Unix()), false)
+	logs, err = Run("AddIndexes", "testdata", "../../..", int(time.Now().Unix()), false)
+	assert.NoError(t, err)
 	pkg.ExpectLines(t, logs, []string{"1621746406-AddIndexes.go", "1621746406-AddIndexes.go"}...)
 }
